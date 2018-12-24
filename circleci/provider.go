1417571265645ad0c1b9d025365760e66d8281e4
Matteo Rolla <matteo@rolla.io>
2018-12-24 15:31:40 +0100

    run gofmt -s

--- circleci/provider.go
+++ circleci/provider.go
@@ -8,19 +8,19 @@ import (
 func Provider() terraform.ResourceProvider {
 	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
-			"api_token": &schema.Schema{
+			"api_token": {
 				Type:        schema.TypeString,
 				Required:    true,
 				DefaultFunc: schema.EnvDefaultFunc("CIRCLECI_TOKEN", nil),
 				Description: "The token key for API operations.",
 			},
-			"vcs_type": &schema.Schema{
+			"vcs_type": {
 				Type:        schema.TypeString,
 				Optional:    true,
 				DefaultFunc: schema.EnvDefaultFunc("CIRCLECI_VCS_TYPE", "github"),
 				Description: "The VCS type for the organization.",
 			},
-			"organization": &schema.Schema{
+			"organization": {
 				Type:        schema.TypeString,
 				Required:    true,
 				DefaultFunc: schema.EnvDefaultFunc("CIRCLECI_ORGANIZATION", nil),
